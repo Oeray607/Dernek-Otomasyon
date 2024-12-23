f23b5c3f0b2f885e5e0901e11b36eba055bef2de
Eray Okuklu <eray_okuklu@hotmail.com>
2024-12-24 01:07:10 +0300

    Dosyalar Yüklendi

---
+++ GörselProgramlamaFinal/FonksiyonK.cs
@@ -0,0 +1,227 @@
+﻿using GörselProgramlamaFinal.Entities;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Layout;
+using System.Net.Mail;
+using System.Net;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Runtime.Remoting.Contexts;
+
+
+namespace GörselProgramlamaFinal
+{
+    internal class FonksiyonK
+    {
+        private VeriErisimK VeriErisimK = new VeriErisimK(); 
+
+        public void UyeEkle(Uye uye)
+        {
+            VeriErisimK VeriErisimK = new VeriErisimK();
+            if (VeriErisimK.UyeVarMi(uye.ID))
+            {
+                throw new Exception("Bu ID'ye sahip bir üye zaten mevcut. Lütfen farklı bir ID kullanın.");
+            }
+    
+            VeriErisimK.UyeEkle(uye); 
+        }
+
+
+        public bool UyeVarMi(int id)
+        {
+            return VeriErisimK.UyeVarMi(id);
+        }
+
+        public void UyeSil(int uyeID)
+        {
+            
+            VeriErisimK.UyeSil(uyeID); 
+        }
+
+        public List<Uye> UyeListele()
+        {
+            return VeriErisimK.UyeListele();
+        }
+
+        public void UyeGuncelle(Uye uye)
+        {
+            VeriErisimK.UyeGuncelle(uye);
+        }
+
+        public Uye UyeIDileGetir(int uyeID)
+        {
+            return VeriErisimK.UyeIDileGetir(uyeID); 
+        }
+
+        public List<string> UyeEmailListele()
+        {
+            return VeriErisimK.UyeEmailListele(); 
+        }
+
+        public Uye UyeBilgileriniGetir(int id)
+        {
+            return VeriErisimK.UyeBilgileriniGetir(id); 
+        }
+        public Uye UyeTumBilgileriniGetir(int id)
+        {
+            return VeriErisimK.UyeTumBilgileriniGetir(id); 
+        }
+
+        public void AidatOde(int id, decimal odemeMiktari)
+        {
+            Uye uye = UyeBilgileriniGetir(id);
+
+            if (uye == null)
+            {
+                throw new Exception("Kullanıcı bulunamadı.");
+            }
+
+            if (odemeMiktari > uye.GüncelBorc)
+            {
+                throw new Exception("Ödeme miktarı güncel borçtan fazla olamaz.");
+            }
+
+            VeriErisimK.AidatOde(id, odemeMiktari); 
+        }
+
+        public void MakbuzOlustur(Uye uye, decimal odemeMiktari)
+        {
+            
+            string pdfPath = $"Makbuz_{uye.AdSoyad}_MakbuzNO_{uye.ID}.pdf";
+
+         
+            using (FileStream stream = new FileStream(pdfPath, FileMode.Create))
+            {
+ 
+                PdfWriter writer = new PdfWriter(stream);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+
+             
+                document.Add(new Paragraph("Aidat Ödeme Makbuzu")
+                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                    .SetFontSize(18));
+
+           
+                document.Add(new Paragraph($"Ad Soyad: {uye.AdSoyad}"));
+                document.Add(new Paragraph($"ID: {uye.ID}"));
+                document.Add(new Paragraph($"Ödenen Tutar: {odemeMiktari:C}")); 
+                document.Add(new Paragraph($"Kalan Güncel Borç: {(uye.GüncelBorc - odemeMiktari):C}"));
+                document.Add(new Paragraph($"Tarih: {DateTime.Now:yyyy-MM-dd HH:mm:ss}"));
+
+               
+                document.Close();
+            }
+
+           
+            MessageBox.Show($"Makbuz oluşturuldu: {pdfPath}", "Makbuz Oluşturma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public List<Uye> BorclariListele()
+        {
+            return VeriErisimK.BorclariListele();
+        }
+
+        public void EtkinlikEpostasiGonder(string etkinlikAdi, string etkinlikYeri, DateTime etkinlikSaati)
+        {
+           
+            List<string> emailListesi = VeriErisimK.UyeEmailListele();
+
+        
+            string mesaj = $"{etkinlikAdi} etkinliğimize herkes davetlidir. Etkinliğimiz {etkinlikSaati} tarihinde  olacaktır. Hepinizi bekliyoruz.<br>Etkinlik Yeri: {etkinlikYeri}.<br>Bu mesaj Kümbetli Köyü Derneği tarafından gönderilmektedir.<br>Toplu mesajdır lütfen cevap vermeyiniz.";
+
+
+
+       
+            foreach (var email in emailListesi)
+            {
+                MailGonder(email, mesaj);  
+            }
+        }
+
+        private void MailGonder(string aliciEmail, string mesaj)
+        {
+
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("eraydernekproje@gmail.com", "wxzevmxwgjtmjmiu"),
+                EnableSsl = true
+            };
+
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress("eraydernekproje@gmail.com"),
+                Subject = "Etkinlik Davetiyesi",
+                Body = mesaj,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(aliciEmail); 
+
+            try
+            {
+                smtpClient.Send(mailMessage);  
+                MessageBox.Show("E-posta başarıyla gönderildi!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"E-posta gönderme hatası: {ex.Message}");
+            }
+        }
+
+        public void DuyuruGonder(string aliciEmail, string mesaj)
+        {
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("eraydernekproje@gmail.com", "wxzevmxwgjtmjmiu"),
+                EnableSsl = true
+            };
+
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress("eraydernekproje@gmail.com"),
+                Subject = "Kümbetli Köyü Derneği",
+                Body = mesaj,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(aliciEmail);
+
+            try
+            {
+                smtpClient.Send(mailMessage); 
+                MessageBox.Show("E-posta başarıyla gönderildi!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"E-posta gönderme hatası: {ex.Message}");
+            }
+        }
+        public void AidatGuncelle(int uyeID, decimal aidat)
+        {
+            VeriErisimK.AidatGuncelle(uyeID, aidat);
+        }
+
+       
+        public void AidatGuncellemeTumu(decimal aidat)
+        {
+            List<Uye> uyeler = VeriErisimK.UyeListele();
+            foreach (var uye in uyeler)
+            {
+                AidatGuncelle(uye.ID, aidat);
+            }
+        }
+
+
+    }
+}
---
+++ GörselProgramlamaFinal/VeriErisimK.cs
@@ -0,0 +1,285 @@
+﻿using GörselProgramlamaFinal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GörselProgramlamaFinal
+{
+    internal class VeriErisimK
+    {
+        private OleDbConnection connect = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Database7.accdb;Persist Security Info=False;");
+
+
+
+        public bool UyeVarMi(int id)
+        {
+            string query = "SELECT COUNT(*) FROM Uyeler WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+            {
+                cmd.Parameters.AddWithValue("@ID", id);
+                connect.Open();
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                connect.Close();
+                return count > 0;
+
+            }
+
+        }
+
+        public void UyeEkle(Uye uye)
+        {
+            string query = "INSERT INTO [Uyeler] ([AdSoyad], [Telefon], [Email], [Adres], [KayıtTarihi], [GüncelBorc], [Aile]) VALUES (@AdSoyad, @Telefon, @Email, @Adres, @KayıtTarihi, @GüncelBorc, @Aile)";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+            cmd.Parameters.Add("@AdSoyad", OleDbType.VarChar).Value = uye.AdSoyad;
+            cmd.Parameters.Add("@Telefon", OleDbType.VarChar).Value = uye.Telefon;
+            cmd.Parameters.Add("@Email", OleDbType.VarChar).Value = uye.Email;
+            cmd.Parameters.Add("@Adres", OleDbType.VarChar).Value = uye.Adres;
+            cmd.Parameters.Add("@KayıtTarihi", OleDbType.Date).Value = uye.KayıtTarihi;
+            cmd.Parameters.Add("@GüncelBorc", OleDbType.Currency).Value = uye.GüncelBorc;
+            cmd.Parameters.Add("@Aile", OleDbType.VarChar).Value = uye.Aile;
+
+
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        public void UyeSil(int uyeID)
+        {
+            string query = "DELETE FROM Uyeler WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+            cmd.Parameters.AddWithValue("@ID", uyeID);
+
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        public List<Uye> UyeListele()
+        {
+            List<Uye> uyeler = new List<Uye>();
+            string query = "SELECT * FROM Uyeler"; 
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+
+            try
+            {
+                connect.Open();  
+                OleDbDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows) 
+                {
+                    while (reader.Read())
+                    {
+                        Uye uye = new Uye
+                        {
+                            ID = Convert.ToInt32(reader["ID"]),
+                            AdSoyad = reader["AdSoyad"].ToString(),
+                            Telefon = reader["Telefon"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Adres = reader["Adres"].ToString(),
+                            KayıtTarihi = Convert.ToDateTime(reader["KayıtTarihi"]),
+                            GüncelBorc = Convert.ToDecimal(reader["GüncelBorc"]),
+                            Aile = reader["Aile"].ToString()
+                        };
+                        uyeler.Add(uye);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Veritabanında üye bulunamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veritabanına bağlanırken bir hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                connect.Close(); 
+            }
+
+            return uyeler;
+        }
+
+        public void UyeGuncelle(Uye uye)
+        {
+            
+                string query = "UPDATE Uyeler SET AdSoyad = @AdSoyad, Telefon = @Telefon, Email = @Email, " +
+                               "Adres = @Adres, KayıtTarihi = @KayıtTarihi, GüncelBorc = @GüncelBorc, " +
+                               "Aile = @Aile WHERE ID = @ID";
+
+                OleDbCommand cmd = new OleDbCommand(query, connect);
+            cmd.Parameters.Add("@AdSoyad", OleDbType.VarChar).Value = uye.AdSoyad;
+            cmd.Parameters.Add("@Telefon", OleDbType.VarChar).Value = uye.Telefon;
+            cmd.Parameters.Add("@Email", OleDbType.VarChar).Value = uye.Email;
+            cmd.Parameters.Add("@Adres", OleDbType.VarChar).Value = uye.Adres;
+            cmd.Parameters.Add("@KayıtTarihi", OleDbType.Date).Value = uye.KayıtTarihi;
+            cmd.Parameters.Add("@GüncelBorc", OleDbType.Currency).Value = uye.GüncelBorc;
+            cmd.Parameters.Add("@Aile", OleDbType.VarChar).Value = uye.Aile;
+            cmd.Parameters.AddWithValue("@ID", uye.ID); 
+
+                connect.Open();
+                cmd.ExecuteNonQuery();
+                connect.Close();
+            
+        }
+
+        public Uye UyeIDileGetir(int ID)
+        {
+            Uye uye = null;
+            string query = "SELECT * FROM Uyeler WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+            cmd.Parameters.AddWithValue("@ID", ID);
+
+            connect.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read()) 
+            {
+                uye = new Uye
+                {
+                    ID = Convert.ToInt32(reader["ID"]),
+                    AdSoyad = reader["AdSoyad"].ToString(),
+                    Telefon = reader["Telefon"].ToString(),
+                    Email = reader["Email"].ToString(),
+                    Adres = reader["Adres"].ToString(),
+                    KayıtTarihi = Convert.ToDateTime(reader["KayıtTarihi"]),
+                    GüncelBorc = Convert.ToDecimal(reader["GüncelBorc"]),
+                    Aile = reader["Aile"].ToString()
+                };
+            }
+
+            connect.Close();
+            return uye;
+        }
+
+        public Uye UyeTumBilgileriniGetir(int ID)
+        {
+            Uye uye = null;
+            string query = "SELECT * FROM Uyeler WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+
+            cmd.Parameters.AddWithValue("@ID", ID);
+
+            connect.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                uye = new Uye
+                {
+                    AdSoyad = reader["AdSoyad"].ToString(),
+                    GüncelBorc = Convert.ToDecimal(reader["GüncelBorc"]),
+                    Telefon = reader.IsDBNull(reader.GetOrdinal("Telefon")) ? null : reader["Telefon"].ToString(),
+                    Email = reader.IsDBNull(reader.GetOrdinal("Email")) ? null : reader["Email"].ToString(),
+                    Adres = reader.IsDBNull(reader.GetOrdinal("Adres")) ? null : reader["Adres"].ToString(),
+                    Aile = reader.IsDBNull(reader.GetOrdinal("Aile")) ? null : reader["Aile"].ToString()
+                };
+            }
+
+            connect.Close();
+            return uye;
+        }
+
+
+        public Uye UyeBilgileriniGetir(int ID)
+        {
+            Uye uye = null;
+            string query = "SELECT * FROM Uyeler WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+
+            cmd.Parameters.AddWithValue("@ID", ID);
+
+            connect.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                uye = new Uye
+                {
+                    ID = Convert.ToInt32(reader["ID"]),
+                    AdSoyad = reader["AdSoyad"].ToString(),
+                    GüncelBorc = Convert.ToDecimal(reader["GüncelBorc"])
+                };
+            }
+
+            connect.Close();
+            return uye;
+        }
+
+        public void AidatOde(int ID, decimal odemeMiktari)
+        {
+            string query = "UPDATE Uyeler SET GüncelBorc = GüncelBorc - @OdemeMiktari WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+
+            cmd.Parameters.AddWithValue("@OdemeMiktari", odemeMiktari);
+            cmd.Parameters.AddWithValue("@ID", ID);
+
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        public List<Uye> BorclariListele()
+        {
+            List<Uye> uyeler = new List<Uye>();
+            string query = "SELECT AdSoyad, Telefon, GüncelBorc FROM Uyeler";
+
+            using (OleDbCommand cmd = new OleDbCommand(query, connect))
+            {
+                connect.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Uye uye = new Uye
+                    {
+                        AdSoyad = reader["AdSoyad"].ToString(),
+                        Telefon = reader["Telefon"].ToString(),
+                        GüncelBorc = Convert.ToDecimal(reader["GüncelBorc"])
+                    };
+                    uyeler.Add(uye);
+                }
+                connect.Close();
+            }
+
+            return uyeler;
+        }
+
+        public List<string> UyeEmailListele()
+        {
+            List<string> emailListesi = new List<string>();
+            string query = "SELECT Email FROM Uyeler";  
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+
+            connect.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                emailListesi.Add(reader["Email"].ToString());  
+            }
+
+            connect.Close();
+            return emailListesi;  
+        }
+
+        public void AidatGuncelle(int uyeID, decimal aidat)
+        {
+            string query = "UPDATE Uyeler SET GüncelBorc = GüncelBorc + @Aidat WHERE ID = @ID";
+            OleDbCommand cmd = new OleDbCommand(query, connect);
+            cmd.Parameters.AddWithValue("@Aidat", aidat);
+            cmd.Parameters.AddWithValue("@ID", uyeID);
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
+        }
+
+
+    }
+}
+
+
